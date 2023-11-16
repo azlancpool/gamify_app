71713485218b13d97e41b37caf8b218108787394
Cristian Naranjo <cnaranjo@payvalida.com>
2023-11-16 13:24:32 -0500

    7. Header images

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import "package:flutter/material.dart";
+import "package:gamify_app/data.dart";
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,8 +9,41 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  double _deviceHeight = 0;
+  double _deviceWidth = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Container(),);
+    _deviceHeight = MediaQuery.of(context).size.height;
+    _deviceWidth = MediaQuery.of(context).size.width;
+
+    return Scaffold(
+      body: Stack(children: <Widget>[_featuredgamesWidget()],)
+      );
+  }
+
+  Widget _featuredgamesWidget(){
+    return SizedBox(
+      height: _deviceHeight * 0.5,
+      width: _deviceWidth,
+      child: PageView(
+        scrollDirection: Axis.horizontal,
+        children: featuredGames.map((_game) {
+          return Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                fit: BoxFit.cover,
+                image: NetworkImage(_game.coverImage.url)
+              )
+            )
+          );
+        }).toList(),
+      ),
+    );
   }
 }
