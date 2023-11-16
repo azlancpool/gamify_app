87dc0e6f75a469d467c41f072746f21142acc2c9
Cristian Naranjo <cnaranjo@payvalida.com>
2023-11-16 13:41:03 -0500

    8. Home gradient

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -23,27 +23,51 @@ class _HomePageState extends State<HomePage> {
     _deviceWidth = MediaQuery.of(context).size.width;
 
     return Scaffold(
-      body: Stack(children: <Widget>[_featuredgamesWidget()],)
+      body: Stack(children: <Widget>[
+        _featuredGamesWidget(),
+        _gradientBoxWidget(),
+        ],)
       );
   }
 
-  Widget _featuredgamesWidget(){
+  Widget _featuredGamesWidget(){
     return SizedBox(
       height: _deviceHeight * 0.5,
       width: _deviceWidth,
       child: PageView(
         scrollDirection: Axis.horizontal,
-        children: featuredGames.map((_game) {
+        children: featuredGames.map((e) {
           return Container(
             decoration: BoxDecoration(
               image: DecorationImage(
                 fit: BoxFit.cover,
-                image: NetworkImage(_game.coverImage.url)
+                image: NetworkImage(e.coverImage.url)
               )
             )
           );
         }).toList(),
       ),
+    );
+  }
+
+  Widget _gradientBoxWidget(){
+    return Align(
+      alignment: Alignment.bottomCenter,
+      child: Container(
+        height: _deviceHeight * 0.8,
+        width: _deviceWidth,
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color.fromRGBO(35, 45, 59, 1.0),
+              Colors.transparent,
+            ],
+            stops:  [0.65, 1.0],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+          )
+        ),
+      )
     );
   }
 }
